4e5221f859134175da94fc38c3056ff50d2fccc5
atem valery ojong <154833460+Atem1972@users.noreply.github.com>
2026-04-27 22:23:10 +0200

    Add variables for Terraform configuration

---
+++ variables.tf
@@ -0,0 +1,35 @@
+locals {
+  install_script = file("${path.module}/install.sh")
+}
+
+
+variable "environment" {
+    type = string
+    default = "dev"
+  
+}
+
+variable "region" {
+  type = string
+  default = "us-east-1"
+}
+
+variable "VPC_cidr" {
+  type = string
+  default = "192.168.0.0/16" 
+}
+
+variable "public_subnet_cidr" {
+  type = string
+  default = "192.168.1.0/24"
+}  
+
+variable "AZ" {
+  type = string
+  default = "us-east-1a"
+}
+
+variable "project-name" {
+  type    = string
+  default = "Docker-swarm"
+}
